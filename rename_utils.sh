78e630705a995bb74997f6ed9382dbc6692e4654
Vy Hong <contact@vyhong.me>
2018-01-13 10:11:40 -0800

    Add rfile utility

--- rename_utils.sh
+++ rename_utils.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Rename the input filename to lowercase.
-function low() {
+function do_rename() {
     fname=$1
-    newname=$(echo "$fname" | tr "[:upper:]" "[:lower:]")
+    newname=$2
     if [ "$fname" != "$newname" ]; then
         mv "$fname" "$newname"
     fi
+}
+
+function get_lowercase_name() {
+    echo "$1" | tr "[:upper:]" "[:lower:]"
+}
+
+# Rename the input filename to lowercase.
+function low() {
+    fname=$1
+    newname=$(get_lowercase_name "$fname")
+    do_rename "$fname" "$newname"
 }
 
 # Rename all files in current directory to lowercase.
@@ -17,14 +27,16 @@ function lowdir() {
     done
 }
 
+function get_name_with_underscore() {
+    # replace unwanted chars with "_", then replace continuous "_"
+    echo "$1" | sed "s/[ -]/_/g" | sed "s/_\+/_/g"
+}
+
 # Replace " " & "-" with "_".
 function under() {
     fname=$1
-    # replace unwanted chars with "_", then replace continuous "_"
-    newname=$(echo "$fname" | sed "s/[ -]/_/g" | sed "s/_\+/_/g")
-    if [ "$fname" != "$newname" ]; then
-        mv "$fname" "$newname"
-    fi
+    newname=$(get_name_with_underscore $fname)
+    do_rename "$fname" "$newname"
 }
 
 # do under() for all files in current directory
@@ -35,8 +47,18 @@ function underdir() {
     done
 }
 
+# Rename a single file.
+function rfile() {
+    fname=$1
+    newname=$(get_name_with_underscore "$fname")
+    newname=$(get_lowercase_name "$newname")
+    do_rename "$fname" "$newname"
+}
+
 # Rename dir.
-function redir() {
-    underdir
-    lowdir
+function rdir() {
+    for fname in *
+    do
+        rfile "$fname"
+    done
 }
